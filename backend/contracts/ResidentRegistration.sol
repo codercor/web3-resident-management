3cd6aa0bd44e2c8799340eb826beb1fffa32a9cf
codercor <mmustafaa9@gmail.com>
2023-07-29 23:42:58 +0300

    new modifiers and functions

--- backend/contracts/ResidentRegistration.sol
+++ backend/contracts/ResidentRegistration.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
-import "./../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 struct Address {
     string street;
@@ -26,27 +26,52 @@ contract ResidentRegistration is Ownable {
     ResidentManagement public residentManagement;
     uint public taxRate = 10;
 
+    modifier shouldExist() {
+        require(
+            residentManagement.residentList[msg.sender].age != 0,
+            "Resident not registered"
+        );
+        _;
+    }
+
+    modifier shouldNotExist() {
+        require(
+            residentManagement.residentList[msg.sender].age == 0,
+            "Resident already registered"
+        );
+        _;
+    }
+
+    modifier isAdult() {
+        require(
+            residentManagement.residentList[msg.sender].age >= 18,
+            "The resident is not adult"
+        );
+        _;
+    }
+
     constructor() {
         residentManagement.totalResident = 0;
     }
 
     // owner functions
-    //set tax rate
     function setTaxRate(uint _taxRate) public onlyOwner {
         taxRate = _taxRate;
     }
 
-    function getAllResident() public view returns (address[] memory) {
+    function getAllResident() public view onlyOwner returns (address[] memory) {
         return residentManagement.residentAddresses;
     }
 
-    function getResident(address _addr) public view returns (Resident memory) {
+    function getResident(
+        address _addr
+    ) public view onlyOwner returns (Resident memory) {
         return residentManagement.residentList[_addr];
     }
 
     function searchResidentByName(
         string memory _name
-    ) public view returns (Resident memory) {
+    ) public view onlyOwner returns (Resident memory) {
         for (uint i = 0; i < residentManagement.residentAddresses.length; i++) {
             if (
                 keccak256(
@@ -67,110 +92,78 @@ contract ResidentRegistration is Ownable {
         }
         revert("Resident not found");
     }
+
     // owner functions end
 
-      // resident functions
+    // resident functions
+    //"selami", 19,"sokak", "samsun", "mahalle", 55800
+    function registerResident(
+        string memory _name,
+        uint _age,
+        string memory _street,
+        string memory _city,
+        string memory _state,
+        uint _zip
+    ) public shouldNotExist {
+        residentManagement.residentList[msg.sender].name = _name;
+        residentManagement.residentList[msg.sender].age = _age;
+        residentManagement.residentList[msg.sender].income = 0;
+        residentManagement.residentList[msg.sender].taxDept = 0;
+        residentManagement.residentList[msg.sender].addr.street = _street;
+        residentManagement.residentList[msg.sender].addr.city = _city;
+        residentManagement.residentList[msg.sender].addr.state = _state;
+        residentManagement.residentList[msg.sender].addr.zip = _zip;
+        residentManagement.residentAddresses.push(msg.sender);
+        residentManagement.totalResident++;
+    }
 
-      function registerResident(
-         string memory _name,
-         uint _age,
-         uint _income,
-         string memory _street,
-         string memory _city,
-         string memory _state,
-         uint _zip
-       ) public {
-         require(
-           residentManagement.residentList[msg.sender].age == 0,
-           "Resident already registered"
-         );
-         residentManagement.residentList[msg.sender].name = _name;
-         residentManagement.residentList[msg.sender].age = _age;
-         residentManagement.residentList[msg.sender].income = _income;
-         residentManagement.residentList[msg.sender].addr.street = _street;
-         residentManagement.residentList[msg.sender].addr.city = _city;
-         residentManagement.residentList[msg.sender].addr.state = _state;
-         residentManagement.residentList[msg.sender].addr.zip = _zip;
-         residentManagement.residentAddresses.push(msg.sender);
-         residentManagement.totalResident++;
-       }
-
-         function updateResident(
-            string memory _name,
-            uint _age,
-            uint _income,
-            string memory _street,
-            string memory _city,
-            string memory _state,
-            uint _zip
-         ) public {
-            require(
+    function updateResident(
+        string memory _name,
+        uint _age,
+        string memory _street,
+        string memory _city,
+        string memory _state,
+        uint _zip
+    ) public shouldExist {
+        require(
             residentManagement.residentList[msg.sender].age != 0,
             "Resident not registered"
-            );
-            residentManagement.residentList[msg.sender].name = _name;
-            residentManagement.residentList[msg.sender].age = _age;
-            residentManagement.residentList[msg.sender].income = _income;
-            residentManagement.residentList[msg.sender].addr.street = _street;
-            residentManagement.residentList[msg.sender].addr.city = _city;
-            residentManagement.residentList[msg.sender].addr.state = _state;
-            residentManagement.residentList[msg.sender].addr.zip = _zip;
-         }
+        );
+        residentManagement.residentList[msg.sender].name = _name;
+        residentManagement.residentList[msg.sender].age = _age;
+        residentManagement.residentList[msg.sender].addr.street = _street;
+        residentManagement.residentList[msg.sender].addr.city = _city;
+        residentManagement.residentList[msg.sender].addr.state = _state;
+        residentManagement.residentList[msg.sender].addr.zip = _zip;
+    }
 
-         function declareIncome(uint _income) public {
-            require(
+    function declareIncome(uint _income) public shouldExist isAdult {
+        residentManagement.residentList[msg.sender].income += _income;
+        uint tax = (_income * taxRate) / 100;
+        residentManagement.residentList[msg.sender].taxDept += tax;
+    }
+
+    function payTax() public payable shouldExist isAdult returns (uint) {
+        require(
             residentManagement.residentList[msg.sender].age != 0,
             "Resident not registered"
-            );
-            residentManagement.residentList[msg.sender].income = _income;
-            uint tax = (_income * taxRate) / 100;
-            residentManagement.residentList[msg.sender].taxDept += tax;
-         }
-
-         function payTax() public payable {
-            require(
-            residentManagement.residentList[msg.sender].age != 0,
-            "Resident not registered"
-            );
-            require(
+        );
+        require(
             residentManagement.residentList[msg.sender].taxDept != 0,
             "No tax dept"
-            );
-            require(
+        );
+        require(
             residentManagement.residentList[msg.sender].taxDept <= msg.value,
             "Insufficient amount"
-            );
-            residentManagement.residentList[msg.sender].taxDept - msg.value;
-         }
+        );
+        residentManagement.residentList[msg.sender].taxDept -= msg.value;
+        return msg.value;
+    }
 
+    //login as resident
+    function getResident() public view shouldExist returns (Resident memory) {}
 
-         //  refund unnecessary tax
-         function refundTax(address _addr, uint _amount) public onlyOwner {
-            require(
-            residentManagement.residentList[_addr].age != 0,
-            "Resident not registered"
-            );
-            require(
-            residentManagement.residentList[_addr].taxDept != 0,
-            "No tax dept"
-            );
-            require(
-            residentManagement.residentList[_addr].taxDept >= _amount,
-            "Insufficient amount"
-            );
-            payable(_addr).transfer(_amount);
-            residentManagement.residentList[_addr].taxDept - _amount;
-         }
-
-         //login as resident
-
-         function loginAsResident() public view returns (Resident memory) {
-            require(
-            residentManagement.residentList[msg.sender].age != 0,
-            "Resident not registered"
-            );
-            return residentManagement.residentList[msg.sender];
-         }
-
-
+    function getTaxDept() public view shouldExist returns (uint) {
+        return residentManagement.residentList[msg.sender].taxDept;
+    }
 }
